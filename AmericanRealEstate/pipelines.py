f46f2e32eb991060056d5ed2fca41f688752a862
changhui.li <1807103336@qq.com>
2019-03-28 09:00:42 +0800

    add json size check

--- AmericanRealEstate/pipelines.py
+++ AmericanRealEstate/pipelines.py
@@ -23,12 +23,16 @@ class RealtorListStoredByServerPipeline(object):
         post_data = {
             "data": data
         }
-        result = RealtorListStoredByServerPipeline.list_session.post(url=realtor_list_post_interface_url, json=json.dumps(post_data))
+        list_json_data = json.dumps(post_data)
+        print("list json data 没有去除空格的大小", len(list_json_data))
+        list_json_data_strip = list_json_data.strip()
+        print("list json data 去除空格的大小", len(list_json_data_strip))
+        result = RealtorListStoredByServerPipeline.list_session.post(url=realtor_list_post_interface_url, json=list_json_data_strip)
 
     def process_item(self, item, spider):
         if isinstance(item, RealtorListPageJsonItem):
             self.house_list.append(json.loads(item['jsonData']))
-            if len(self.house_list) >= 5 or len(spider.start_urls) == 0:
+            if len(self.house_list) >= 20 or len(spider.start_urls) == 0:
                 print('list 数据列表已经达到要求，开始发送数据到服务器')
                 time_now = time.time()
                 self.post_data_to_server(self.house_list)
@@ -48,7 +52,11 @@ class RealtorDetailStoredByServerPipeline(object):
         post_data = {
             "data": data
         }
-        result = RealtorDetailStoredByServerPipeline.detail_session.post(url=realtor_detail_post_interface_url, json=json.dumps(post_data))
+        detail_json_data = json.dumps(post_data)
+        print("list json data 没有去除空格的大小", len(detail_json_data))
+        detail_json_data_strip = detail_json_data.strip()
+        print("list json data 去除空格的大小", len(detail_json_data_strip))
+        result = RealtorDetailStoredByServerPipeline.detail_session.post(url=realtor_detail_post_interface_url, json=detail_json_data_strip)
 
     def process_item(self, item, spider):
         if isinstance(item, RealtorDetailPageJsonItem):
